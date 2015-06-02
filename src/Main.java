1897f3efa02537678e846e27566333f23f9fa556
alaikhtman <alaikhtman@gmail.com>
2015-06-02 22:35:34 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,15 @@ public class Main
             //connector.selectByphone("156");
             //connector.selectByName("dasha");
 
-            ArrayList<String> emails = connector.getAllemails();
-            for(String e: emails)
-            {
-                System.out.println(e);
-            }
-            connector.disconnect();
+//            ArrayList<String> emails = connector.getAllemails();
+//            for(String e: emails)
+//            {
+//                System.out.println(e);
+//            }
+//            connector.disconnect();
+
+           // connector.removeByPhone("0000");
+            connector.updateByPhone("957", "taras", "463463", "ttt@");
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
